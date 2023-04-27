0361d6f6400a118250995155c0ab2d0555c3f54d
ethan <66395264+friendofgats@users.noreply.github.com>
2023-04-27 18:19:14 -0500

    defaults on pageload

--- src/components/Navmenu.jsx
+++ src/components/Navmenu.jsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from "react";
 import HoverableImage from "./shared/HoverableImage";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import AssetLoader from "./shared/AssetLoader";
 
 
-export default function Navmenu() {
+export default function Navmenu(props) {
+    const pathsToImages = {
+        "": "me",
+        "work": "work",
+        "fun": "fun",
+        "contact": "contact",
+        "site": "site"
+    }
+
     const [images, setImages] = useState({});
     const [gifs, setGifs] = useState({});
-    const [section, setSection] = useState("");
+    const [section, setSection] = useState(pathsToImages[useLocation().pathname.replace("/", "")]);
     const [avatar, setAvatar] = useState("");
 
     function toggleAvatar(identifier) {
@@ -22,8 +30,9 @@ export default function Navmenu() {
     useEffect(() => {
         setImages(AssetLoader(require.context('../assets/images/global', false, /\.(png|jpe?g|svg)$/)));
         setGifs(AssetLoader(require.context('../assets/gifs/global', false, /\.(gif)$/)));
-    }, [])
 
+
+    }, [])
     return (
         <header className="z-10 w-full">
             <div className="flex max-2xl:flex-col 2xl:flex-wrap 2xl:p-5 items-center justify-center 2xl:justify-evenly text-xl hover:text-yellow">
@@ -36,18 +45,18 @@ export default function Navmenu() {
                             e.currentTarget.src.substring(e.currentTarget.src.lastIndexOf('/') + 1).split('.')[0])}
                     />
                 </div>
-                <Link className="object-cover title-font 2xl:ml-5 max-2xl:mb-10 justify-center items-center" to="http://localhost:3000/me">
-                    <HoverableImage image={toggleSectionImage("name")} alt="Ethan Heyrman" hoverImage={gifs.name} onClick={() => setSection("name")} />
+                <Link className="object-cover title-font 2xl:ml-5 max-2xl:mb-10 justify-center items-center" to={""}>
+                    <HoverableImage image={toggleSectionImage("me")} alt="Ethan Heyrman" hoverImage={gifs.me} onClick={() => setSection("me")} />
                 </Link>
                 <img className="object-cover max-2xl:hidden 2xl:visible" src={images.break} alt="|" />
                 <div className="flex flex-wrap items-center max-2xl:mb-5 max-2xl:justify-center">
-                    <Link className="object-cover" to="http://localhost:3000/work">
+                    <Link className="object-cover" to="/work">
                         <HoverableImage image={toggleSectionImage("work")} alt="What I do for work" hoverImage={gifs.work} onClick={() => setSection("work")} />
                     </Link>
-                    <Link className="object-cover m-5" to="http://localhost:3000/hobbies">
+                    <Link className="object-cover m-5" to="/fun">
                         <HoverableImage image={toggleSectionImage("fun")} alt="What I do for fun" hoverImage={gifs.fun} onClick={() => setSection("fun")} />
                     </Link>
-                    <Link className="object-cover" to="http://localhost:3000/contact">
+                    <Link className="object-cover" to="/contact">
                         <HoverableImage image={toggleSectionImage("contact")} alt="Contact me" hoverImage={gifs.contact} onClick={() => setSection("contact")} />
                     </Link>
                 </div>
--- src/components/shared/RequestHandler.jsx
+++ src/components/shared/RequestHandler.jsx
@@ -4,33 +4,26 @@ import { useState, useEffect } from "react";
 const BASE_URL = "http://localhost:8000/info"
 
 export default function RequestHandler(props = { section: "", verbosity: 0 }) {
-    const [data, setData] = useState({});
+    const [data, setData] = useState([]);
     const [loaded, setLoaded] = useState(false);
-    const [error, setError] = useState("");
+    const [error, setError] = useState(null);
 
     const { section, verbosity } = props;
-    const key = section + '_' + verbosity;
 
     useEffect(() => {
-        // localStorage will not work in private browser sessions
-        setData(JSON.parse(localStorage.getItem(key)));
-        if (Object.keys(data).length === 0) {
-            axios.get(BASE_URL, {
-                params: {
-                    section: section,
-                    verbosity: verbosity
-                }
+        axios.get(BASE_URL, {
+            params: {
+                section: section,
+                verbosity: verbosity
             }
-            ).then((response) => {
-                localStorage.setItem(key, JSON.stringify(response.data))
-                setData(response.data)
-
-            })
-                .catch(() => {
-                    setError("Error retrieving data. Sorry you had to see this.")
-                });
         }
-        setLoaded(true);
+        ).then((response) => {
+            setLoaded(true);
+            setData(response.data)
+        })
+            .catch(() => {
+                setError("Error retrieving data. Sorry you had to see this.")
+            });
     }, []);
     return { data, error, loaded };
 }
