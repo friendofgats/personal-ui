ae54141d92c6cecbad76ef90c02216a1e2facbfb
ethan <66395264+friendofgats@users.noreply.github.com>
2023-05-27 01:15:42 -0500

    low rent robo callers... get real

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,6 +4,7 @@ import Container from "./shared/Container";
 import RequestHandler from './shared/RequestHandler'
 export default function Contact({ section = "Contact", verbosity = 1, setVerbosity }) {
     const [images, setImages] = useState({});
+    const [phone, setPhone] = useState("CLICK ME!");
 
     useEffect(() => {
         setImages(AssetLoader(require.context('../assets/images/contact', false, /\.(png|jpe?g|svg)$/)));
@@ -21,18 +22,18 @@ export default function Contact({ section = "Contact", verbosity = 1, setVerbosi
             <Container className="-mt-5" body={data?.body} />
             <hr className="w-full h-1 mx-auto my-8 bg-yellow border-0" />
             <div className="text-xl xl:text-3xl text-yellow">
-                <div className="flex pr-3 pl-3 pb-3 items-center">
+                <div className="flex pr-3 pl-3 pb-3 items-center hover:text-orange">
                     <img className="w-10 h-13" src={images.github} alt="github" />
                     <a className="pl-5" href="https://www.github.com/friendofgats">github.com/friendofgats</a>
                 </div>
-                <div className="flex p-3 items-center">
+                <div className="flex p-3 items-center hover:text-orange">
                     <img className="w-10 h-13" src={images.phone} alt="phone" />
-                    <p className="pl-5">+1.920.660.7195</p>
+                    <p className="pl-5" onClick={() => { setPhone("+1.920.660.7195"); this.onClick = null; }}>{phone}</p>
 
                 </div>
-                <div className="flex p-3 items-center">
+                <div className="flex p-3 items-center hover:text-orange">
                     <img className="w-10 h-13" src={images.email} alt="email" />
-                    <p className="pl-5">ethan.heyrman@outlook.com</p>
+                    <a className="pl-5" href="mailto:ethanheyrman.dev@outlook.com">ethanheyrman.dev@outlook.com</a>
                 </div>
             </div>
         </div>
