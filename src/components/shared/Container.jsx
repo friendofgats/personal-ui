d114c6bf9acd73621a3a6df865ae5701d4be44ff
ethan <66395264+friendofgats@users.noreply.github.com>
2023-05-29 15:47:33 -0500

    container was behaving strangely

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -3,7 +3,7 @@
  * @param {String} body Text to render
  */
 export default function Container({ body = "" }) {
-    const text = body?.split('\n').map(e => <p className='pt-5' key={e}>{e}</p>)
+    const text = body?.split('\n').map((e, index) => <p className='pt-5' key={index}>{e}</p>)
     return (
         <div className="text-xl xl:text-3xl max-2xl:ml-2 max-2xl:mr-2 text-yellow">
             {text}
